d445e17de36d9088f45108dc3a4c05cfc325c716
Ugo Cipriani <ciprianu@f202-11.e-im2ag.ujf-grenoble.fr>
2019-12-17 10:28:10 +0100

    Fin 2eme partie de l'etape3

--- etape3.c
+++ etape3.c
@@ -1,27 +1,42 @@
 #include "etape3.h"
 
 uint32_t* read_data(char* fname, int nsection) {
-  FILE* file = fopen(fname, "r");
-  ElfHeader elf_header;
-  fread(&elf_header, sizeof(elf_header), 1, file);
+  Elf_SecHeaderF* elf_sec_header = etape2(fname);
 
-  fseek(file, reverse_endian_32(elf_header.shoff) + nsection*reverse_endian_16(elf_header.shentsize), SEEK_SET);
+  uint32_t* data = NULL;
 
-  Elf_SecHeader elf_sec_header;
-  fread(&elf_sec_header, sizeof(elf_sec_header), 1, file);
+  if(elf_sec_header[nsection]) {
+    fseek(file, elf_sec_header[nsection].offset, SEEK_SET);
 
-  fseek(file, reverse_endian_32(elf_sec_header.offset), SEEK_SET);
+    data = malloc(elf_sec_header[nsection].size);
+    fread(data, elf_sec_header[nsection].size, 1, file);
 
-  uint32_t* data = malloc(reverse_endian_32(elf_sec_header.size));
-  fread(data, reverse_endian_32(elf_sec_header.size), 1, file);
-
-  for (int i = 0; i < reverse_endian_32(elf_sec_header.size)/sizeof(uint32_t); i++)
-      printf("%08X\n", reverse_endian_32(data[i]));
+    for (int i = 0; i < elf_sec_header[nsection].size/sizeof(uint32_t); i++)
+        printf("%08X\n", reverse_endian_32(data[i]));
+  }
 
   return data;
 }
 
 uint32_t* read_data(char* fname, char* nsection) {
-  Elf_SecHeaderF elf_sec_header = etape2(fname);
-  
+  Elf_SecHeaderF* elf_sec_header = etape2(fname);
+
+  int i = 0;
+  while(elf_sec_header[i] && !strcmp(elf_sec_header[i].nameStr, nsection)) {
+    i++;
+  }
+
+  uint32_t* data = NULL;
+
+  if(elf_sec_header[i]) {
+    fseek(file, elf_sec_header[i].offset, SEEK_SET);
+
+    data = malloc(elf_sec_header[i].size);
+    fread(data, elf_sec_header[i].size, 1, file);
+
+    for (int j = 0; j < elf_sec_header[i].size/sizeof(uint32_t); j++)
+        printf("%08X\n", reverse_endian_32(data[j]));
+  }
+
+  return data;
 }
