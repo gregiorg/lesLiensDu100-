6cd6c24588a4be0d21a67745f739b870c184cd4c
francis <francis@cabrel.com>
2019-12-17 13:54:54 +0100

    ajout de specification.h

---
+++ specification.h
@@ -0,0 +1,20 @@
+/*
+Ce fichier indique quels sont les regles de formatages et d'organisation du code a respecter
+*/
+
+NomDeStruct
+pas d'underscore
+toujours typedef les struct
+
+nomDeVariable
+pas d'underscore
+
+type nomDeFonction(arguments) {
+}
+pas d'underscore
+
+#define __GARDE_DE_HEADER_H__
+
+passer des FILE* plutot que les char* des noms de fichiers
+
+passer les struct par pointeur plutot que par valeur
