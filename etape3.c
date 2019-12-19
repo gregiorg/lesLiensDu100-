d93749f0ae23999d21e1ff0f6ed3c9e0ffebe1cd
Jean-Edouard Bournat-Querat <bournatj@im2ag-turing2.u-ga.fr>
2019-12-19 18:37:10 +0100

    commentaires

--- etape3.c
+++ etape3.c
@@ -1,6 +1,9 @@
+//------------------------AFFICHAGE DU CONTENU D'UNE SECTION-------------------
 #include "etape3.h"
 
+//A partir du numéro de section
 uint32_t* readDataNumSec(FILE* f, int numSection) {
+  long int filePos = ftell(f);
   ElfHeaderF* elfHeader = getElfHeader(f);
   ElfSecHeaderF** elfSecHeader = getTabElfSecHeader(f);
 
@@ -10,12 +13,24 @@ uint32_t* readDataNumSec(FILE* f, int numSection) {
     fseek(f, elfSecHeader[numSection]->offset, SEEK_SET);
 
     data = malloc(elfSecHeader[numSection]->size);
-    fread(data, elfSecHeader[numSection]->size, 1, f);
+    size_t codeRet = fread(data, elfSecHeader[numSection]->size, 1, f);
+    if(codeRet != 1) {
+        if (feof(f)){
+          printf("Erreur de lecture du fichier: fin de fichier inattendue\n");
+          exit(EXIT_FAILURE);
+        } else if (ferror(f)) {
+          perror("Erreur de lecture du fichier");
+          exit(EXIT_FAILURE);
+        }
+    }
   }
+  fseek(f, filePos, SEEK_SET);
   return data;
 }
 
+//A partir du nom de section
 uint32_t* readDataNomSec(FILE* f, char* nomSection) {
+  long int filePos = ftell(f);
   ElfHeaderF* elfHeader = getElfHeader(f);
   ElfSecHeaderF** elfSecHeader = getTabElfSecHeader(f);
 
@@ -30,11 +45,22 @@ uint32_t* readDataNomSec(FILE* f, char* nomSection) {
     fseek(f, elfSecHeader[i]->offset, SEEK_SET);
 
     data = malloc(elfSecHeader[i]->size);
-    fread(data, elfSecHeader[i]->size, 1, f);
+    size_t codeRet = fread(data, elfSecHeader[i]->size, 1, f);
+    if(codeRet != 1) {
+        if (feof(f)){
+          printf("Erreur de lecture du fichier: fin de fichier inattendue\n");
+          exit(EXIT_FAILURE);
+        } else if (ferror(f)) {
+          perror("Erreur de lecture du fichier");
+          exit(EXIT_FAILURE);
+        }
+    }
   }
+  fseek(f, filePos, SEEK_SET);
   return data;
 }
 
+//Affichage des données
 void afficherDataSection(uint32_t* data, ElfSecHeaderF** elfSecHeader, int numSection) {
   for (int i = 0; i < elfSecHeader[numSection]->size/sizeof(uint32_t); i++) {
       printf("%08X\n", reverseEndian32(data[i]));
