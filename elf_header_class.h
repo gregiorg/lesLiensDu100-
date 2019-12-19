de1a7ef52a720e0fef28d9a318b7b4fb11454bb3
rcmz0 <antoinerichermoz@gmail.com>
2019-12-19 14:11:58 +0100

    le systeme objet est fonctionel

--- elf_header_class.h
+++ elf_header_class.h
@@ -1,30 +1,8 @@
-typedef struct {
-    uint8_t indentClass;
-    uint8_t indentData;
-    uint8_t indentVersion;
-    uint16_t type;
-    uint16_t machine;
-    uint32_t version;
-    uint32_t entry;
-    uint32_t flags;
-    SectionHeaderTable* sectionHeaderTable;
-} Header;
+#ifndef __HEADER_CLASS_H__
+#define __HEADER_CLASS_H__
 
-Header* headerFromFile(FILE* file);
-void headerWriteToFile(Header*, FILE* file);
-//char* headerGetString(int index);
-
-typedef struct {
-    SectionHeader** sections;
-    int nbSections;
-    Header* header;
-} SectionHeaderTable;
-
-int sectionHeaderTableGetSize(SectionHeaderTable*);
-SectionHeader** sectionHeaderTableGetSectionsOfType(SectionHeaderTable*, uint32_t, int*);
-SectionHeader** sectionHeaderTableGetSectionsWithName(SectionHeaderTable*, char*, int*);
-//SectionHeader* sectionHeaderTableGetSectionOfType(SectionHeaderTable*, uint32_t);
-//SectionHeader* sectionHeaderTableGetSectionWithName(SectionHeaderTable*, char*);
+#include <stdio.h>
+#include "util.h"
 
 typedef struct {
 	unsigned int indexTable;
@@ -36,19 +14,41 @@ typedef struct {
 	uint32_t info;
 	uint32_t addrAlign;
 	uint32_t entSize;
+	uint32_t size;
     void* data;
 } SectionHeader;
 
-const char* sectionHeaderGetNameString(SectionHeader*);
-const char* sectionHeaderGetTypeString(SectionHeader*);
-int sectionHeaderGetSize(SectionHeader*);
-RelocationTable* sectionHeaderGetRelocationTable(SectionHeader*);
-SymboleTable* sectionHeaderGetSymboleTable(SectionHeader*);
-
 typedef struct {
-    SymboleTableEntry**;
-    int nbrEntry;
-} SymboleTable;
+    uint8_t indentClass;
+    uint8_t indentData;
+    uint8_t indentVersion;
+    uint16_t type;
+    uint16_t machine;
+    uint32_t version;
+    uint32_t entry;
+    uint32_t flags;
+    int nbSections;
+    SectionHeader** sections;
+} Header;
+
+Header* headerFromFile(FILE* file);
+void headerWriteToFile(Header* header, FILE* file);
+
+//void headerPrint(Header*);
+//void headerWriteToFile(Header*, FILE* file);
+//char* headerGetString(int index);
+
+//int sectionHeaderTableGetSize(SectionHeaderTable*);
+//SectionHeader** sectionHeaderTableGetSectionsOfType(SectionHeaderTable*, uint32_t, int*);
+//SectionHeader** sectionHeaderTableGetSectionsWithName(SectionHeaderTable*, char*, int*);
+//SectionHeader* sectionHeaderTableGetSectionOfType(SectionHeaderTable*, uint32_t);
+//SectionHeader* sectionHeaderTableGetSectionWithName(SectionHeaderTable*, char*);
+
+//const char* sectionHeaderGetNameString(SectionHeader*);
+//const char* sectionHeaderGetTypeString(SectionHeader*);
+//int sectionHeaderGetSize(SectionHeader*);
+//RelocationTable* sectionHeaderGetRelocationTable(SectionHeader*);
+//SymboleTable* sectionHeaderGetSymboleTable(SectionHeader*);
 
 typedef struct {
     char* name;
@@ -61,11 +61,8 @@ typedef struct {
 } SymboleTableEntry;
 
 typedef struct {
-    RelocationTableEntry**;
-    int nbrEntry;
-} RelocationTable;
-
-typedef struct {
     SymboleTableEntry* sym;
     char type;
 } RelocationTableEntry;
+
+#endif
