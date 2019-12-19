975158ee31e971327093eedcb458c46e7352caf6
rcmz0 <antoinerichermoz@gmail.com>
2019-12-19 17:10:50 +0100

    Ajout elf_header_class

--- test_elf_header_class.c
+++ test_elf_header_class.c
@@ -2,17 +2,17 @@
 
 int main(int argc, char** argv) {
 	FILE* file1 = fopen(argv[1], "r");
-	FILE* file2 = fopen(argv[2], "w");
+//	FILE* file2 = fopen(argv[2], "w");
 
-	if (file1 == NULL || file2 == NULL) {
+	if (file1 == NULL ) { // || file2 == NULL) {
 		return 1;
 	}
 
 	Header* header = headerFromFile(file1);
-	headerWriteToFile(header, file2);
-
+//	headerWriteToFile(header, file2);
+	
 	fclose(file1);
-	fclose(file2);
+//	fclose(file2);
 
 	return 0;
 }
