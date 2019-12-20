76a37ab665c22562564446dd9519deae181d23b8
Ait gueni ssaid Abderrahmane <aaitguenissiad@gmail.com>
2019-12-20 15:07:11 +0100

    errno etape4.c

--- test_etape4.c
+++ test_etape4.c
@@ -1,28 +1,37 @@
 #include "etape4.h"
+#include <errno.h>
+
+extern int errno;
+
 
 int main(int argc, char* argv[]) {
 
-  FILE* f = fopen(argv[1], "r");
+  FILE* file = fopen(argv[1], "r");
+  if (file == NULL) {
+    fprintf(stderr, "Value of errno: %d\n", errno);
+    fprintf(stderr, "Error opening the file: %s\n", strerror( errno ));
+    perror("Error printed by perror");
+    exit(EXIT_FAILURE);
+  }
 
-  ElfHeaderF* elfHeader = getElfHeader(f);
-  ElfSecHeaderF** elfSecHeader = getTabElfSecHeader(f);
+  ElfHeaderF* elfHeader = getElfHeader(file);
+  ElfSecHeaderF** elfSecHeader = getTabElfSecHeader(file);
   int i = 0;
   while(i < elfHeader->shnum && strcmp(elfSecHeader[i]->nameStr, ".symtab")!=0) {
     i++;
   }
   int nbElm = (elfSecHeader[i]->size) / (elfSecHeader[i]->entSize);
 
-  int indexOfStrTab = 0;
-  while(indexOfStrTab < elfHeader->shnum && strcmp(elfSecHeader[indexOfStrTab]->nameStr, ".strtab")!=0) {
-    indexOfStrTab++;
+  int a = 0;
+  while(a < elfHeader->shnum && strcmp(elfSecHeader[a]->nameStr, ".strtab")!=0) {
+    a++;
   }
-  
-  uint32_t stringTableAddress = getAddressStringTable(elfHeader->shoff, elfHeader->shentsize, indexOfStrTab, f);
+  uint32_t stringTableAddress = getAddressStringTable(elfHeader->shoff, elfHeader->shentsize, elfSecHeader[a]->offset, file);
 
-  Elf32Sym** tabSym = getTabSym(f);
-  afficherTabSym(tabSym, nbElm, stringTableAddress, f);
+  Elf32Sym** tabSym = getTabSym(file);
+  afficherTabSym(tabSym, nbElm, stringTableAddress, file);
 
-  fclose(f);
+  fclose(file);
 
   return 0;
 }
