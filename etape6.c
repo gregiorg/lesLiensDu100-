e049f074e346688fcb9cc585b3285d37e6ee8c41
Gregiorg <gergely.fodor@etu.univ-grenoble-alpes.fr>
2020-01-07 10:48:03 +0100

    finalisation de l'etape 6

--- etape6.c
+++ etape6.c
@@ -2,6 +2,7 @@
 
 void fusionProgbit(FILE* file1, FILE* file2, FILE* file3) {
 
+  // extracting the elf informations with legolas
   Header* h1 = legolasReadFromFile(file1);
   Header* h2 = legolasReadFromFile(file2);
 
@@ -15,9 +16,11 @@ void fusionProgbit(FILE* file1, FILE* file2, FILE* file3) {
       for (int j = 0; j < h1->shnum; j++) {
           SectionHeader* sh1 = h1->sectionHeaderTable[j];
 
+          // if they are both progbits and they have the same name
           if (sh1->type == SHT_PROGBITS && sh2->type == SHT_PROGBITS && strcmp(sh1->name, sh2->name)==0) {
               progbitFusion = 1;
 
+              // reallocating more memory space and copying data
               sh1->rawData = realloc(sh1->rawData, sh1->size + sh2->size);
               memcpy(((char*) sh1->rawData) + sh1->size, sh2->rawData, sh2->size);
               sh1->size += sh2->size;
