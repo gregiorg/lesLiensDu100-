c52dc8f0630e27f3e09af59a229d6d11204e727a
aaitguenissaid <aaitguenissaid@gmail.com>
2020-01-09 10:33:20 +0100

    open et close de tous les fichiers dans les main, UPDATE testetape8.c

--- test_etape8.c
+++ test_etape8.c
@@ -3,15 +3,18 @@
 int main(int argc, const char* argv[]) {
     const char* fName1 = argv[1];
     const char* fName2 = argv[2];
-    const char* fName3 = argv[3];
+    const char* fNameR = argv[3];
 
     FILE* file1 = fopenR(fName1, "r");
     FILE* file2 = fopenR(fName2, "r");
+    FILE* fileR = fopenR(fNameR, "r");
 
-    fusionRelocationTable(file1, file2, fName3);
+
+    fusionRelocationTable(file1, file2, fileR);
 
     fclose(file1);
     fclose(file2);
+    fclose(fileR);
 
     return 0;
 }
