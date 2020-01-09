8d43e5f51f686eebe680f1990be23e8e5ba5ab91
aaitguenissaid <aaitguenissaid@gmail.com>
2020-01-09 10:10:38 +0100

    testetape?.c update

--- test_etape3.c
+++ test_etape3.c
@@ -5,12 +5,6 @@ extern int errno;
 int main(int argc, char* argv[]) {
 
   FILE* file = fopenR(argv[1], "r");
-  if (file == NULL) {
-		fprintf(stderr, "Value of errno: %d\n", errno);
-		fprintf(stderr, "Error opening the file: %s\n", strerror( errno ));
-		perror("Error printed by perror");
-		exit(EXIT_FAILURE);
-	}
 
   ElfHeader* elfHeaderEndian = malloc(sizeof(ElfHeader));
   freadElfHEader(elfHeaderEndian, sizeof(*elfHeaderEndian), 1, file);
