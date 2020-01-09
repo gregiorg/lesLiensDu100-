65ea38a0d7d548edba5dfafa1540cab2cdc2d72c
francis <francis@cabrel.com>
2020-01-09 09:42:55 +0100

    separation legolas finie

--- Makefile
+++ Makefile
@@ -2,26 +2,27 @@ CC = gcc
 CFLAGS = -g -Wall -Werror
 SOURCES=$(wildcard *.c)
 OBJ=$(patsubst %.c, %.o, $(SOURCES))
+LEGOLAS_OBJ=legolas.o legolas_header.o legolas_section.o legolas_string_table.o legolas_symbole_table.o legolas_relocation_table.o
 OBJECTS = test_etape8 test_etape7 test_etape6 test_etape5 test_etape4 test_etape3 test_etape2 test_etape1
 
 all: $(OBJECTS)
 
 test_etape8: $(OBJ)
-	$(CC) $(CFLAGS) -o $@ $@.o etape8.o legolas.o util.o
+	$(CC) $(CFLAGS) -o $@ $@.o $(LEGOLAS_OBJ) etape8.o util.o
 test_etape7: $(OBJ)
-	$(CC) $(CFLAGS) -o $@ $@.o etape7.o legolas.o util.o
+	$(CC) $(CFLAGS) -o $@ $@.o $(LEGOLAS_OBJ) etape7.o util.o
 test_etape6: $(OBJ)
-	$(CC) $(CFLAGS) -o $@ $@.o etape6.o legolas.o util.o
+	$(CC) $(CFLAGS) -o $@ $@.o $(LEGOLAS_OBJ) etape6.o util.o
 test_etape5: $(OBJ)
-	$(CC) $(CFLAGS) -o $@ $@.o etape5.o etape4.o etape3.o etape2.o etape1.o util.o
+	$(CC) $(CFLAGS) -o $@ $@.o $(LEGOLAS_OBJ) etape5.o etape4.o etape3.o etape2.o etape1.o util.o
 test_etape4: $(OBJ)
-	$(CC) $(CFLAGS) -o $@ $@.o etape4.o etape3.o etape2.o etape1.o util.o
+	$(CC) $(CFLAGS) -o $@ $@.o $(LEGOLAS_OBJ) etape4.o etape3.o etape2.o etape1.o util.o
 test_etape3: $(OBJ)
-	$(CC) $(CFLAGS) -o $@ $@.o etape3.o etape2.o etape1.o util.o
+	$(CC) $(CFLAGS) -o $@ $@.o $(LEGOLAS_OBJ) etape3.o etape2.o etape1.o util.o
 test_etape2: $(OBJ)
-	$(CC) $(CFLAGS) -o $@ $@.o etape2.o etape1.o util.o
+	$(CC) $(CFLAGS) -o $@ $@.o $(LEGOLAS_OBJ) etape2.o etape1.o util.o
 test_etape1: $(OBJ)
-	$(CC) $(CFLAGS) -o $@ $@.o etape1.o util.o
+	$(CC) $(CFLAGS) -o $@ $@.o $(LEGOLAS_OBJ) etape1.o util.o
 
 %.o: %.c
 	$(CC) $(CFLAGS) -c $<
--- legolas.h
+++ legolas.h
@@ -13,13 +13,91 @@ typedef struct SectionHeader SectionHeader;
 typedef struct SymboleTableEntry SymboleTableEntry;
 typedef struct RelocationTableEntry RelocationTableEntry;
 
+struct Header {
+    uint8_t indentClass;
+    uint8_t indentData;
+    uint8_t indentVersion;
+    uint8_t indentOSABI;
+    uint8_t indentABIVersion;
+    uint16_t type;
+    uint16_t machine;
+    uint32_t version;
+    uint32_t entry;
+    uint32_t flags;
+    int shnum;
+    SectionHeader** sectionHeaderTable;
+};
+
+struct SectionHeader {
+	char* name;
+	uint32_t type;
+	uint32_t flags;
+	uint32_t addr;
+	uint32_t link;
+	uint32_t info;
+	uint32_t addrAlign;
+	uint32_t size;
+    uint32_t entSize;
+    uint32_t nbEntry;
+    void* rawData;
+    union {
+		char* stringTable;
+		SymboleTableEntry** symboleTable;
+		RelocationTableEntry** relocationTable;
+	} data;
+};
+
+struct SymboleTableEntry {
+    char* name;
+    uint32_t value;
+    uint32_t size;
+    char bind;
+    char type;
+    char other;
+    SectionHeader* sectionHeader;
+};
+
+struct RelocationTableEntry {
+    SymboleTableEntry* sym;
+    uint32_t offset;
+    char type;
+};
+
 Header* legolasReadFromFile(FILE* file);
 void legolasWriteToFile(Header* header, FILE* file);
 
-#include "legolas_header.h"
-#include "legolas_section.h"
-#include "legolas_string_table.h"
-#include "legolas_symbol_table.h"
-#include "legolas_relocation_table.h"
+void headerAddSection(Header*, SectionHeader*);
+void headerRemoveSectionHeader(Header*, SectionHeader*);
+int headerGetIndexOfSectionHeader(Header*, SectionHeader*);
+SectionHeader* getSectionHeaderFromName(Header*, char*);
+SectionHeader** getSectionHeadersFromType(Header*, uint32_t, size_t*);
+void setHeader(Elf32_Ehdr*, Header*);
+
+void typeFirstRawDataPartIfNeeded(SectionHeader*, Header*);
+void typeLastRawDataPartIfNeeded(SectionHeader*, Header*);
+SectionHeader* getSectionHeaderAddress(Header* header, uint16_t shndx);
+char* sectionHeaderGetData(SectionHeader*);
+uint32_t sectionHeaderGetEntSize(SectionHeader*);
+void setSectionHeader(Header*, Elf32_Shdr*, int, FILE*);
+char* sectionHeaderGetRawData(SectionHeader*);
+
+void stringTableAddString(SectionHeader*, char*);
+int stringTableGetIndex(SectionHeader*, char*);
+char* stringTableGetString(SectionHeader*, int);
+SectionHeader* stringTableCreate(char*);
+
+SymboleTableEntry* getSymboleTableEntryAddress(Header*, uint32_t);
+char* getSymbolTableEntryName(Header* header, uint32_t);
+void changeSymbolTableEntryPointerOnSectionHeaderOnFusion(Header*, SectionHeader*, SectionHeader*);
+void symbolTableAddGlobalEntry(SectionHeader*, SymboleTableEntry*, unsigned int);
+void symbolTableAddLocalEntry(SectionHeader*, SymboleTableEntry*, unsigned int);
+void symboleTableRemoveEntry(SectionHeader*, SymboleTableEntry*);
+Elf32_Sym* sectionHeaderGetSymbolData(Header*, SectionHeader*, SectionHeader*);
+int symbolTableGetEntryIndex(SectionHeader*, SymboleTableEntry*);
+
+void updateRelocationTableEntryOffsetOnSectionHeaderOnFusion(Header*, SectionHeader*, SectionHeader*);
+void relocationTableAddEntry(SectionHeader*, RelocationTableEntry*);
+Elf32_Rel* sectionHeaderGetUnexplicitRelocationData(SectionHeader*, SectionHeader*);
+Elf32_Rela* sectionHeaderGetExplicitRelocationData(SectionHeader*);
 
 #endif
